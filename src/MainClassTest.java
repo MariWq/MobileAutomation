b409e988564921369437fcad1d4fca21188d2eee
biruchkovamari <biruchkovamari@gmail.com>
2020-11-25 21:30:40 +0400

    add second test

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -11,4 +11,13 @@ public class MainClassTest extends MainClass{
             System.out.println("number is not equal to 14");
         }
     }
+
+    @Test
+    public void testGetClassNumber(){
+        if (getClassNumber() >= 45 ){
+            System.out.println("class_number more than 45");
+        }else{
+            System.out.println("class_number no more than 45");
+        }
+    }
 }
